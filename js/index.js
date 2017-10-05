6aa42daa458869805a4c7e0b3123ebb7f5718913
Zach Overhulser <zoverlvx@do4.localdomain>
2017-10-05 16:19:29 +0000

    Separating js from index.html

--- js/index.js
+++ js/index.js
@@ -1,4 +1,47 @@
-var markdown = require( "markdown" ).markdown;
-fs = require('fs');
-md = fs.readFileSync("quotes.md").toString();
-console.log( markdown.toHTML( md) );
+// var markdown = require( "markdown" ).markdown;
+// fs = require('fs');
+// md = fs.readFileSync("quotes.md").toString();
+// console.log( markdown.toHTML( md) );
+
+
+(function () {
+    let markdown = require("markdown").markdown;
+    fs = require("fs");
+    md = fs.readFileSync("quotes.md").toString();
+    console.log(markdown.toHTML(md)); 
+
+    function Editor(input, preview) {
+        this.update = function () {
+            preview.innerHTML = markdown.toHTML(input.value);
+        };
+        input.editor = this;
+        this.update();
+    }
+
+    function Selector(div, fileObjects) {
+        this.update = function () {
+            div.innerHTML = makeFileLinks();
+        };
+        this.update();
+    }
+
+    function makeFileLinks() {
+        html = "<a href=\'/\'>fileId001</a>";
+        html += "<br>";
+        html += "<a href=\'/\' class='selected'>fileId002</a>";
+        html += "<br>";
+        html += "<a href=\'/\'>fileId002</a>";
+        return html;
+    }
+
+    const $ = function (id) { return document.getElementById(id); };
+    new Editor($("text-input"), $("preview"));
+
+    new Selector($("selector"),
+        [{name:'interface.md',
+        path:'./'
+        }]
+    );
+
+
+}());
